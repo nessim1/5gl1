6b651ddf3729315f52de29d539768245f689a73d
user <user@HP-PC>
2015-11-29 22:38:41 +0100

    team commit
    Signed-off-by: user <user@HP-PC>

---
+++ firstMaven/src/main/java/com/esprit/App.java
@@ -0,0 +1,13 @@
+package com.esprit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
